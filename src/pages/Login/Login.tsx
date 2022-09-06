bb2654687061e9e0c57fc5d830404d67395ada89
mimoon5860 <mahmodulislammoon@gmail.com>
2022-09-06 23:07:49 +0600

    form done

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import './login.css';
 const Login = () => {
+    const [loginInfo, setLoginInfo] = useState<{ phone: string, password: string } | {}>({});
+
+    const handleLogin = (event: React.FormEvent): void => {
+        event.preventDefault();
+
+    }
     return (
         <div className='bg-wrapper'>
             <Container fluid>
                 <Row>
                     <Col className='mx-auto bg-white p-2 mt-3 rounded-3 ' md={4}>
-                        <Form>
+                        <h3>Login form</h3>
+                        <Form onSubmit={handleLogin}>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Label>Phone number</Form.Label>
+                                <Form.Control onChange={(e) => setLoginInfo({ ...loginInfo, phone: e.target.value })} type="text" placeholder="Enter your phone" />
                                 <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
+                                    We'll never share your number with anyone else.
                                 </Form.Text>
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control onChange={(e) => setLoginInfo({ ...loginInfo, password: e.target.value })} type="password" placeholder="Password" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                                 <Form.Check type="checkbox" label="Check me out" />
                             </Form.Group>
                             <Button variant="primary" type="submit">
-                                Submit
+                                Login
                             </Button>
                         </Form>
                     </Col>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -4,46 +4,39 @@ import './register.css';
 
 const Register = () => {
     const [profilePhoto, setProfilePhoto] = useState<File | null>(null);
-
+    const [registerInfo, setRegisterInfo] = useState<{ name: string, phone: string, email: string, password: string } | {}>({})
 
     return (
         <div className='bg-wrapper'>
             <Container>
+                <div className='text-center'>
+                    <img width={200} src="/assets/chatter_logo.png" alt="" />
+                </div>
                 <Row>
                     <Col className='mx-auto bg-white p-2 mt-3 rounded-3' md={4}>
-                        <div className='text-center'>
-                            <label htmlFor="profileImage">
-                                {
-                                    profilePhoto ? <img width={150} height={150} className="rounded-circle" src={URL.createObjectURL(profilePhoto)} alt="" /> :
-                                        <img width={150} height={150} className="rounded-circle" src="/assets/avatar-image.png" alt="" />
-                                }
-                                <br />
-                                <input onChange={(event) => event.target.files && setProfilePhoto(event.target.files[0])} id="profileImage" name="profileImage" className='d-none' type="file" />
-                                <span className='upload-photo-btn mt-3'>Upload photo</span>
-                            </label>
-                        </div>
+                        <h3>Registration form</h3>
                         <Form>
                             <Form.Group className="mb-3" controlId="formBasicName">
                                 <Form.Label>Full name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your full name" />
+                                <Form.Control onChange={(e) => setRegisterInfo({ ...registerInfo, name: e.target.value })} type="text" placeholder="Enter your full name" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicPhone">
                                 <Form.Label>Phone number</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your phone" />
+                                <Form.Control onChange={(e) => setRegisterInfo({ ...registerInfo, phone: e.target.value })} type="text" placeholder="Enter your phone" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Control onChange={(e) => setRegisterInfo({ ...registerInfo, email: e.target.value })} type="email" placeholder="Enter email" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control onChange={(e) => setRegisterInfo({ ...registerInfo, password: e.target.value })} type="password" placeholder="Password" />
                             </Form.Group>
                             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                                 <Form.Check type="checkbox" label="Check me out" />
                             </Form.Group>
                             <Button variant="primary" type="submit">
-                                Submit
+                                Register
                             </Button>
                         </Form>
                     </Col>
